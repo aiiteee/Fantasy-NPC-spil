6c89bde8c211344a3e3d9792efa922cc40e6dda5
aiiteee <pernille.mikkelsen@outlook.dk>
2024-04-19 22:10:20 +0200

    NPC Animations virker

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,44 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    /*[field: SerializeField] public float MoveForce { get; private set; } = 250f;
-
-    [field: SerializeField] public AnimationClip Back { get; private set; }
-    [field: SerializeField] public AnimationClip Front { get; private set; }
-    [field: SerializeField] public AnimationClip Left { get; private set; }
-    [field: SerializeField] public AnimationClip Right { get; private set; }
-    [field: SerializeField] public AnimationClip Idle { get; private set; }
-
-    private Vector2 axisInput = Vector2.zero;
-    private Rigidbody2D rb;
-    private Animator animator;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        AnimationClip = Idle;
-    }
-
-    private void FixedUpdate()
-    {
-        Vector2 moveForce = axisInput * MoveForce * Time.fixedDeltaTime;
-        rb.AddForce(moveForce);
-    }*/
-
     
-
-
-
-
-
-
-
-
-
-
-
-
 
     public float moveSpeed;
     public Rigidbody2D rb;
