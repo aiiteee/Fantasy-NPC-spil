904e8c97d05c926eeebc40e0df636e18e0b01259
aiiteee <pernille.mikkelsen@outlook.dk>
2024-04-25 16:14:55 +0200

    save

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,21 +6,30 @@ public class Item : MonoBehaviour
 {
     public AudioClip clip;
     public float volume = 1;
-    public bool canPickUp;
+    public bool canPickUp=false;
     public QuestManager questManager;
+    public DialogueTrigger dialogueTrigger;
+    public GameObject pickUpcue;
+
+    
 
     // Update is called once per frame
     void Update()
     {
         if(canPickUp)
         {
-            if (Input.GetKeyDown(KeyCode.F)
+            pickUpcue.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.F))
             {
                 AudioSource.PlayClipAtPoint(clip, transform.position, volume);
                 Destroy(gameObject);
                 questManager.currentAmount += 1;
             }
 
+        }
+        else
+        {
+            pickUpcue.SetActive(false);
         }
     }
     
@@ -29,7 +38,7 @@ public class Item : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")&&dialogueTrigger.questBegun)
         {
             canPickUp = true;
         }
--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -9,8 +9,11 @@ public class QuestManager : MonoBehaviour
 
     //public string questText1;
     //public string questText2;
-    public int goalAmount;
-    public int currentAmount;
+
+    
+
+    public float goalAmount;
+    public float currentAmount;
 
     public TMP_Text lineOneText;
     public TMP_Text lineTwoText;
@@ -22,6 +25,8 @@ public class QuestManager : MonoBehaviour
     public GameObject returnToQuest;
 
     public DialogueTrigger dialogueTrigger;
+
+    public bool questTwoBegun;
 
     
     
@@ -67,6 +72,23 @@ public class QuestManager : MonoBehaviour
         lineTwoText.SetText("Gather 3 brightbloom flowers ("+currentAmount+"/"+goalAmount+")");
         FirstMarkX.SetActive(true);
         SecondMarkX.SetActive(true);
+        questTwoBegun = true;
+
+        
+    }
+
+    void Update()
+    {
+        
+        if(questTwoBegun)
+        {
+            lineTwoText.SetText("Gather 3 brightbloom flowers (" + currentAmount + "/" + goalAmount+")");
+            if (currentAmount == goalAmount)
+            {
+                SecondMarkY.SetActive(true);
+                SecondMarkX.SetActive(false);
+            }
+        }
     }
 
 }
