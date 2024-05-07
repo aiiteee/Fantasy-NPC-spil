c6c2628851ca44743ceed6e0149e0790fa612f82
aiiteee <pernille.mikkelsen@outlook.dk>
2024-05-07 11:46:07 +0200

    fixes

--- Assets/BranchSystem.cs
+++ Assets/BranchSystem.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class BranchSystem : MonoBehaviour
 {
-    //make static later when tests are finished
+    //point der holder styr på ens tidligere valg.
+    //statiske variabler er for at beholde information mellem scener.
     public static float björnPoints = 0;
     public static float ranusPoints = 0;
 
+    //et point til Björn
     public void BjörnPoint()
     {
         björnPoints += 1;
     }
 
+    //et point til Ranus
     public void RanusPoint()
     {
         ranusPoints += 1;
     }
 }
+
+
+
+
